f6b98185a1719db000ca2799f5623b926bd35ca0
John Shelby <johnshelby@yahoo.com>
2017-03-19 22:17:01 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+var sortedRobots = robots.map(robot=>{
+  let dec = knownDecepticons.includes(robot.name)
+  return Object.assign({}, robot, {
+    alliance: dec ? 'decepticon' : 'autobot'
+  });
+});
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +28,8 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+var coloredZebraStripes = zebraStripes.map((stripe,index)=>{
+  return Object.assign({}, stripe, {color: index % 2 ? 'black' : 'white'
+  });
+})
